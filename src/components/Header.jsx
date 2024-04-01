73c2cc18776bb7a8495163883fb2c1c9c8796969
shubham-mishra-2003 <shubhammishra3070@gmail.com>
2024-04-01 14:27:29 +0530

    Added adscense

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,25 @@ import SavedCodes from "./SavedCodes";
 
 const Header = () => {
   return (
-    <header className="w-full p-2 pl-4 flex justify-between items-center border-b-8 border-blue-500 bg-black text-white">
-      <div className="flex gap-3 items-center">
-        <img
-          src={Logo}
-          className="rounded-full border-white w-10 h-10 md:w-14 md:h-14"
-        />
-        <div className="flex w-min flex-col md:text-xl text-sm font-bold text-blue-500">
-          <span>Connect.CodeCraft</span>
-          <span className="md:text-lg text-[0.9em] text-gray-400 self-start">
-            By shubham mishra
-          </span>
+    <header className="w-full flex flex-col justify-between items-center bg-black text-white">
+      <div className="w-full items-start flex border-b-2 max-h-16">Ads</div>
+      <div className="flex w-full justify-between items-center border-b-8 p-2 pl-4 pr-1 border-blue-500">
+        <div className="flex gap-3 items-center">
+          <img
+            src={Logo}
+            className="rounded-full border-white w-10 h-10 md:w-14 md:h-14"
+          />
+          <div className="flex w-min flex-col md:text-xl text-sm font-bold text-blue-500">
+            <span>Connect.CodeCraft</span>
+            <span className="md:text-lg text-[0.9em] text-gray-400 self-start">
+              By shubham mishra
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="flex gap-2 items-center">
-        <SavedCodes />
-        <SaveButton />
+        <div className="flex items-center">
+          <SavedCodes />
+          <SaveButton />
+        </div>
       </div>
     </header>
   );
